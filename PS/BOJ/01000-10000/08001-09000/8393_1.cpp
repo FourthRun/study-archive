baf89b629b894f9308484e19a8485b70640257c1
SeongSu Kim <4thrun@naver.com>
2025-12-21 01:18:30 +0900

    feat: [BOJ] 반복문
    - 2739
    - 8393
    - 10950

---
+++ PS/BOJ/01000-10000/08001-09000/8393_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num, result = 0;
+
+    cin >> num;
+
+    for(int i = 1; i <= num; i++){
+        result += i;
+    }
+
+    cout << result;
+
+    return 0;
+}
---
+++ PS/BOJ/01000-10000/08001-09000/8393_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cin >> num;
+
+    // 등차수열 합 공식
+    cout << num * (num + 1) / 2;
+
+    return 0;
+}

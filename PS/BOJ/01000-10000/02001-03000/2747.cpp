54d1b4743a158e030000420412517b3fb0e5a71d
SeongSu Kim <4thrun@naver.com>
2025-12-31 20:28:27 +0900

    feat: [BOJ] 2747.cpp

---
+++ PS/BOJ/01000-10000/02001-03000/2747.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[46] = {0, };
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+    arr[1] = 1;
+
+    for(int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+
+    cout << arr[n];
+
+    return 0;
+}

2823abb192995300472f1f49215011675b4504bc
SeongSu Kim <4thrun@naver.com>
2026-01-13 23:34:07 +0900

    feat: [BOJ] 2440.cpp 등 4개
    - 2440, 2441, 2442
    - 10093

---
+++ PS/BOJ/10001-20000/10001-11000/10093.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, m, a, b, temp;
+
+    cin >> n >> m;
+
+    if(n < m) {
+        a = n;
+        b = m;
+    }
+    else {
+        a = m;
+        b = n;
+    }
+
+    if(a == b || abs(a-b) == 1) {
+        cout << 0;
+    }
+    else {
+        cout << b - a - 1 << "\n";
+
+    for(int i = a + 1; i < b; i++) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}

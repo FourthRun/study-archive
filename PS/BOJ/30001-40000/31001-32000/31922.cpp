182263d394e7757094f8a49f8e1f2f032a158df7
SeongSu Kim <4thrun@naver.com>
2026-02-04 14:03:24 +0900

    feat: [BOJ] solved.ac 랜덤 마라톤 88
    - 3046
    - 31922

---
+++ PS/BOJ/30001-40000/31001-32000/31922.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, p, c;
+
+    cin >> a >> p >> c;
+
+    if(a + c >= p) cout << a + c;
+    else cout << p;
+
+    return 0;
+}

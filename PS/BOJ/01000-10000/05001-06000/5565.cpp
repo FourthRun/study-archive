1c12d5cda6740c55d7c2498b91503c347e0ed667
SeongSu Kim <4thrun@naver.com>
2026-02-07 23:29:50 +0900

    feat: [BOJ] solve 5565

---
+++ PS/BOJ/01000-10000/05001-06000/5565.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sum, num;
+
+    cin >> sum;
+
+    for(int i = 0; i < 9; i++) {
+        cin >> num;
+
+        sum -= num;
+    }
+
+    cout << sum;
+
+    return 0;
+}

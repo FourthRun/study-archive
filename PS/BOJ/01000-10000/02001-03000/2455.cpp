be7add3881be37f81f90fad7ddeb22155837f0d0
SeongSu Kim <4thrun@naver.com>
2026-02-16 22:55:59 +0900

    feat: [BOJ] 잡다한 문제
    - 2399, 2455

---
+++ PS/BOJ/01000-10000/02001-03000/2455.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int in, out;
+    int train = 0;
+    int max = -1;
+
+    for(int i = 0; i < 4; i++) {
+        cin >> out >> in;
+
+        train = train + in - out;
+
+        if(train > max) max = train;
+    }
+
+    cout << max;
+
+    return 0;
+}

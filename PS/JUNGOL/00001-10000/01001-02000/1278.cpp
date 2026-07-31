51bd50380a7ad7c307a2dc077783a95e46a62c30
SeongSu Kim <4thrun@naver.com>
2026-07-31 23:54:07 +0900

    feat: [정올] Intermediate 동적계획법1
    - 1077, 1278

---
+++ PS/JUNGOL/00001-10000/01001-02000/1278.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+int dp[1001][10001];
+
+int main() {
+    int n, w;
+
+    cin >> n >> w;
+
+    vector<pair<int, int>> v(n + 1);
+
+    for(int i = 1; i <= n; ++i) {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= w; ++j) {
+            if(j >= v[i].first) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i].first] + v[i].second);
+            else dp[i][j] = dp[i - 1][j];
+        }
+    }
+
+    cout << dp[n][w];
+
+    return 0;
+}
